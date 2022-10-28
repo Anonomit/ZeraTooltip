baf26f409c1e1d86f6eb28f4d17bbf0bfa89396d
Anonomit <anonomit@gmail.com>
2022-10-28 16:28:50 -0400

    Add item levels to tokens (up to Ulduar)
    These reflect the item level of gear that the token provides.

--- Operations/AddItemLevel.lua
+++ Operations/AddItemLevel.lua
@@ -10,6 +10,21 @@ local strMatch = string.match
 local tinsert  = table.insert
 
 
+local tokenOverrides = {}
+
+for ilvl, ids in pairs({
+  [200] = {40610, 40611, 40612, 40613, 40614, 40615, 40616, 40617, 40618, 40619, 40620, 40621, 40622, 40623, 40624}, -- T7 10m
+  [213] = {40625, 40626, 40627, 40628, 40629, 40630, 40631, 40632, 40633, 40634, 40635, 40636, 40637, 40638, 40639, 44569}, -- T7 25m, Key to the Focusing Iris 10m
+  [219] = {45635, 45636, 45637, 45644, 45645, 45646, 45647, 45648, 45649, 45650, 45651, 45652, 45659, 45660, 45661}, -- T8 10m
+  [226] = {45632, 45633, 45634, 45638, 45639, 45640, 45653, 45654, 45655, 45641, 45642, 45643, 45656, 45657, 45658, 44581, 46052}, -- T8 25m, Key to the Focusing Iris 25m, Reply-Code Alpha 10m
+  [239] = {46053}, -- Reply-Code Alpha 25m
+  [245] = {45038}, -- Fragment of Val'anyr
+}) do
+  for _, id in ipairs(ids) do
+    tokenOverrides[id] = ilvl
+  end
+end
+
 
 
 local stat = "ItemLevel"
@@ -43,8 +58,8 @@ end
 function Addon:AddItemLevel(tooltipData)
   if self:GetOption("hide", stat) then return end
   local equipLoc = select(4, GetItemInfoInstant(tooltipData.id))
-  if equipLoc ~= "" then
-    local itemLevel = select(4, GetItemInfo(tooltipData.id))
+  if equipLoc ~= "" or tokenOverrides[tooltipData.id] then
+    local itemLevel = tokenOverrides[tooltipData.id] or select(4, GetItemInfo(tooltipData.id))
     local color = self:GetDefaultOption("color", stat)
     if self:GetOption("allow", "recolor") and self:GetOption("doRecolor", stat) then
       color = self:GetOption("color", stat)
