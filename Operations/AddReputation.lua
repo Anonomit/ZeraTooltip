1797f27fba4cbb3108ad15b1f70abc438b30d0b5
Anonomit <anonomit@gmail.com>
2024-01-10 20:23:41 -0500

    Add reputation text to waylaid supply items

--- Operations/AddReputation.lua
+++ Operations/AddReputation.lua
@@ -27,9 +27,11 @@ Reputation standings
 
 
 local REP_PATTERN               = " +%d"
+local REP_COUNT_PATTERN         = " +%d x%d = +%d"
 local WAYLAID_PATTERN           = format(" +%%d (%s) | +%%d (%s)", EMPTY or GLYPH_EMPTY or GLYPH_INACTIVE, LOC_TYPE_FULL or LOAD_FULL or SHAKE_INTENSITY_FULL)
 local REPUTATION_CUTOFF_PATTERN = " (|cff%s%s/%s|r)"
 
+local FACTION_ID = Addon.MY_FACTION == "Alliance" and 2586 or 2587
 
 
 local reputationStandings = {}
@@ -45,16 +47,63 @@ end
 Addon.waylaidSupplies = {}
 local reputationItems = {}
 for ids, repInfo in pairs{
-  [{211331, 210771, 211332, 211329, 211315, 211316, 211933, 211317, 211330}]                         = {factionID = Addon.MY_FACTION == "Alliance" and 2586 or 2587, min = 100, cutoff = 5, max = 300},
-  [{211365}]                                                                                         = {factionID = Addon.MY_FACTION == "Alliance" and 2586 or 2587, min = 300, cutoff = 5},
-  [{211327, 211328, 211319, 211326, 211325, 211934, 211321, 211318, 211322, 211324, 211323, 211320}] = {factionID = Addon.MY_FACTION == "Alliance" and 2586 or 2587, min = 100, cutoff = 5, max = 450},
-  [{211367}]                                                                                         = {factionID = Addon.MY_FACTION == "Alliance" and 2586 or 2587, min = 450, cutoff = 5},
-  [{211819, 211822, 211837, 211838, 211821, 211820, 211836, 211835, 211823}]                         = {factionID = Addon.MY_FACTION == "Alliance" and 2586 or 2587, min = 200, cutoff = 6, max = 500},
-  [{211839}]                                                                                         = {factionID = Addon.MY_FACTION == "Alliance" and 2586 or 2587, min = 500, cutoff = 6},
-  [{211831, 211833, 211824, 211828, 211825, 211829}]                                                 = {factionID = Addon.MY_FACTION == "Alliance" and 2586 or 2587, min = 200, cutoff = 6, max = 650},
-  [{211840}]                                                                                         = {factionID = Addon.MY_FACTION == "Alliance" and 2586 or 2587, min = 650, cutoff = 6},
-  [{211935, 211832, 211830, 211834, 211827, 211826}]                                                 = {factionID = Addon.MY_FACTION == "Alliance" and 2586 or 2587, min = 200, cutoff = 6, max = 800},
-  [{211841}]                                                                                         = {factionID = Addon.MY_FACTION == "Alliance" and 2586 or 2587, min = 800, cutoff = 6},
+  [{211331, 210771, 211332, 211329, 211315, 211316, 211933, 211317, 211330}]                         = {factionID = FACTION_ID, min = 100, cutoff = 5, max = 300},
+  [{211365}]                                                                                         = {factionID = FACTION_ID, min = 300, cutoff = 5},
+  [{211327, 211328, 211319, 211326, 211325, 211934, 211321, 211318, 211322, 211324, 211323, 211320}] = {factionID = FACTION_ID, min = 100, cutoff = 5, max = 450},
+  [{211367}]                                                                                         = {factionID = FACTION_ID, min = 450, cutoff = 5},
+  [{211819, 211822, 211837, 211838, 211821, 211820, 211836, 211835, 211823}]                         = {factionID = FACTION_ID, min = 200, cutoff = 6, max = 500},
+  [{211839}]                                                                                         = {factionID = FACTION_ID, min = 500, cutoff = 6},
+  [{211831, 211833, 211824, 211828, 211825, 211829}]                                                 = {factionID = FACTION_ID, min = 200, cutoff = 6, max = 650},
+  [{211840}]                                                                                         = {factionID = FACTION_ID, min = 650, cutoff = 6},
+  [{211935, 211832, 211830, 211834, 211827, 211826}]                                                 = {factionID = FACTION_ID, min = 200, cutoff = 6, max = 800},
+  [{211841}]                                                                                         = {factionID = FACTION_ID, min = 800, cutoff = 6},
+  
+  [{6290}]  = {factionID = FACTION_ID, min = 300, count = 20, cutoff = 5}, -- Brilliant Smallfish
+  [{2840}]  = {factionID = FACTION_ID, min = 300, count = 20, cutoff = 5}, -- Copper Bar
+  [{2581}]  = {factionID = FACTION_ID, min = 300, count = 10, cutoff = 5}, -- Heavy Linen Bandage
+  [{6888}]  = {factionID = FACTION_ID, min = 300, count = 20, cutoff = 5}, -- Herb Baked Egg
+  [{2318}]  = {factionID = FACTION_ID, min = 300, count = 14, cutoff = 5}, -- Light Leather
+  [{2447}]  = {factionID = FACTION_ID, min = 300, count = 20, cutoff = 5}, -- Peacebloom
+  [{2835}]  = {factionID = FACTION_ID, min = 300, count = 10, cutoff = 5}, -- Rough Stone
+  [{765}]   = {factionID = FACTION_ID, min = 300, count = 20, cutoff = 5}, -- Silverleaf
+  [{2680}]  = {factionID = FACTION_ID, min = 300, count = 20, cutoff = 5}, -- Spiced Wolf Meat
+  
+  [{4343}]  = {factionID = FACTION_ID, min = 450, count = 6,  cutoff = 5}, -- Brown Linen Pant
+  [{6238}]  = {factionID = FACTION_ID, min = 450, count = 4,  cutoff = 5}, -- Brown Linen Robe
+  [{2847}]  = {factionID = FACTION_ID, min = 450, count = 6,  cutoff = 5}, -- Copper Shortsword
+  [{2300}]  = {factionID = FACTION_ID, min = 450, count = 3,  cutoff = 5}, -- Embossed Leather Vest
+  [{4237}]  = {factionID = FACTION_ID, min = 450, count = 5,  cutoff = 5}, -- Handstitched Leather Boots
+  [{929}]   = {factionID = FACTION_ID, min = 450, count = 10, cutoff = 5}, -- Healing Potion
+  [{11287}] = {factionID = FACTION_ID, min = 450, count = 2,  cutoff = 5}, -- Lesser Magic Wand
+  [{118}]   = {factionID = FACTION_ID, min = 450, count = 20, cutoff = 5}, -- Minor Healing Potion
+  [{20744}] = {factionID = FACTION_ID, min = 450, count = 2,  cutoff = 5}, -- Minor Wizard Oil
+  [{4362}]  = {factionID = FACTION_ID, min = 450, count = 3,  cutoff = 5}, -- Rough Boomsticks
+  [{4360}]  = {factionID = FACTION_ID, min = 450, count = 12, cutoff = 5}, -- Rough Copper Bomb
+  [{3473}]  = {factionID = FACTION_ID, min = 450, count = 3,  cutoff = 5}, -- Runed Copper Pants
+  
+  [{2841}]  = {factionID = FACTION_ID, min = 500, count = 12, cutoff = 6}, -- Bronze Bar
+  [{2453}]  = {factionID = FACTION_ID, min = 500, count = 20, cutoff = 6}, -- Bruiseweed
+  [{5527}]  = {factionID = FACTION_ID, min = 500, count = 8,  cutoff = 6}, -- Goblin Deviled Clam
+  [{3531}]  = {factionID = FACTION_ID, min = 500, count = 15, cutoff = 6}, -- Heavy Wool Bandage
+  [{2319}]  = {factionID = FACTION_ID, min = 500, count = 12, cutoff = 6}, -- Medium Leather
+  [{2842}]  = {factionID = FACTION_ID, min = 500, count = 6,  cutoff = 6}, -- Silver Bar
+  [{6890}]  = {factionID = FACTION_ID, min = 500, count = 20, cutoff = 6}, -- Smoked Bear Meat
+  [{21072}] = {factionID = FACTION_ID, min = 500, count = 15, cutoff = 6}, -- Smoked Sagefish
+  [{2452}]  = {factionID = FACTION_ID, min = 500, count = 20, cutoff = 6}, -- Swiftthistle
+  
+  [{2316}]  = {factionID = FACTION_ID, min = 650, count = 2,  cutoff = 6}, -- Dark Leather Cloak
+  [{2587}]  = {factionID = FACTION_ID, min = 650, count = 4,  cutoff = 6}, -- Gray Woolen Shirt
+  [{3385}]  = {factionID = FACTION_ID, min = 650, count = 20, cutoff = 6}, -- Lesser Mana Potion
+  [{20745}] = {factionID = FACTION_ID, min = 650, count = 2,  cutoff = 6}, -- Minor Mana Oil
+  [{6350}]  = {factionID = FACTION_ID, min = 650, count = 3,  cutoff = 6}, -- Rough Bronze Boots
+  [{4374}]  = {factionID = FACTION_ID, min = 650, count = 12, cutoff = 6}, -- Small Bronze Bomb
+  
+  [{6373}]  = {factionID = FACTION_ID, min = 800, count = 15, cutoff = 6}, -- Elixir of Firepower
+  [{4251}]  = {factionID = FACTION_ID, min = 800, count = 2,  cutoff = 6}, -- Hillman's Shoulders
+  [{5507}]  = {factionID = FACTION_ID, min = 800, count = 2,  cutoff = 6}, -- Ornate Spyglass
+  [{5542}]  = {factionID = FACTION_ID, min = 800, count = 3,  cutoff = 6}, -- Pearl-clasped Cloak
+  [{6339}]  = {factionID = FACTION_ID, min = 800, count = 1,  cutoff = 6}, -- Runed Silver Rod
+  [{15869}] = {factionID = FACTION_ID, min = 800, count = 14, cutoff = 6}, -- Silver Skeleton Key
 } do
   local faction = GetFactionInfoByID(repInfo.factionID)
   if faction then
@@ -95,6 +144,8 @@ function Addon:RewordReputation(itemID)
   end
   if repInfo.max then
     text = text .. format(WAYLAID_PATTERN, repInfo.min, repInfo.max)
+  elseif repInfo.count then
+    text = text .. format(REP_COUNT_PATTERN, Addon:Round(repInfo.min/repInfo.count, 1), repInfo.count, repInfo.min)
   else
     text = text .. format(REP_PATTERN, repInfo.min)
   end
